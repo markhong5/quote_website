f6372ab2732d8c8ee058b70cb8c480dad511b051
markhong5 <markh5@uci.edu>
2019-11-11 14:53:20 -0800

    reloading the page gives me a random quote

--- flask_site.py
+++ flask_site.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+import pickle
+import random
+quotes_dict = pickle.load(open('quotes_dict2.pkl', 'rb'))
 app = Flask(__name__)
+
+def random_quote():
+    name, message = random.choice(list(quotes_dict.items()))
+    return name, message
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -9,5 +16,6 @@ def hello(name="Mark"):
 
 @app.route('/quote/')
 @app.route('/quote/<name><message>')
-def quote(name='Ashley', message='Hi there'):
+def quote():
+    name, message = random_quote()
     return render_template('quote_page.html', name=name, message=message)
